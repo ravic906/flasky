d2713a4e19fa0fb9a03e36c7511a6b77b1d32936
Ravichandra N <67008019+ravic906@users.noreply.github.com>
2026-02-21 18:50:52 +0530

    Refactor gemini.py for improved readability

--- gemini.py
+++ gemini.py
@@ -1,8 +1,9 @@
 from google import genai
 
-client = genai.Client(api_key="AIzaSyAcm_iysylKKzhEo1gTVgXQMxrH7KaSgwk")
+client = 
 
 response = client.models.generate_content(
     model="gemini-3-flash-preview", contents="Can you read a sql or python file and create documentation?"
 )
+
 print(response.text)
